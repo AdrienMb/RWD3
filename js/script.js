702962ada9d6b67c8d3cb2219c7fd63296721722
AdrienMb <adrien.mbirko@gmail.com>
2018-11-20 14:16:47 +0100

    Shark

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
+$("#sharkId").hide();
+
+function shark (top, left){
+    $("#sharkId").animate({top: 0, left: 0});
+    $("#sharkId").fadeIn().animate({top: top, left: left}).fadeOut();
+}
 
 $("#fish2Id").mouseover( function() {
     var left = Math.floor((Math.random() * ($(window).width()-$("#fish2Id").width()) + 1));
     var top = Math.floor((Math.random() * ($(window).height()-$("#fish2Id").height()) + 1));
     $("#fish2Id").animate({top: top, left: left});
+    var random = Math.floor((Math.random() * 6 + 1));
+    if (random==1){
+        shark(top, left);
+    }
 })
 
